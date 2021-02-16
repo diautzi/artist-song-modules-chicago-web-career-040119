c1bd0e63ace353ca810dbd90c0b90806a2d45eaa
Diana Utan <didiutzi@gmail.com>
2021-02-16 23:47:20 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 require_relative '../lib/modules/findable'
 require_relative '../lib/modules/memorable'
-require_relative '../lib/concerns/paramable'
+require_relative '../lib/modules/paramable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
